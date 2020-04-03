3cd148c971b022478658360c9d50aa662c6d8733
nevor <nevikhun@gmail.com>
2020-04-03 16:13:45 -0300

    1

--- client/js/events.js
+++ client/js/events.js
@@ -1,12 +1,4 @@
 $("body").on("click", "#see_plans", function(){
-    ga('send', {
-        hitType: 'event',
-        eventCategory: 'Consulta click',
-        eventAction: 'click',
-        eventLabel: 'Odisi Health consulta'
-      });
-
-
       //scroll
      
       $('html,body').animate({
