bfdec2468614f1f78f1a9be436130b3e21ddf7b4
nevor <nevikhun@gmail.com>
2020-04-04 07:23:20 -0300

    date added in contact registration

--- handlers/db_habdler.js
+++ handlers/db_habdler.js
@@ -2,6 +2,7 @@ const Consulta = require("../models/contact_model.js");
 
 const db_handler = {
     register: async function(contact, callback){
+        contact.date =  new Date();
         let write = await Consulta.create(contact, callback)
         await write;
         return true;
--- models/contact_model.js
+++ models/contact_model.js
@@ -7,6 +7,7 @@ nombre: {type: String, required: true},
 email: {type: String, required: true},
 empresa : {type: String, required: true},
 plan : {type: String, required: true},
+date: {type: Date, required: true},
 });
 
 var Contact = module.exports = mongoose.model("Odisi2020Contact", Contact);
